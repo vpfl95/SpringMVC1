d6dfd63ad2f938570c5df9f590cd2ba8b606e859
vpfl95 <k_dh610@naver.com>
2023-03-22 17:48:59 +0900

    Initial commit

---
+++ src/test/java/hello/SpringMVC1/SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.SpringMVC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
