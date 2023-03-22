46a91a83c702dd3745dff1b607ed4666c07b0b5a
vpfl95 <k_dh610@naver.com>
2023-03-22 22:09:57 +0900

    HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/hello/SpringMVC1/basic/HelloData.java
@@ -0,0 +1,11 @@
+package hello.SpringMVC1.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class HelloData {
+    private String username;
+    private int age;
+
+}
